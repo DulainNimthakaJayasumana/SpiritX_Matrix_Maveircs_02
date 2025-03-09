9877ad50d4c4e9c34756d09194c3ddac6750f1b6
Akash Welgama <139337308+Akashwelgama@users.noreply.github.com>
2025-03-09 13:43:04 +0530

    player-select-backend-version-05
    Debugged auth points

--- player-select-backend/src/auth/auth.controller.ts
+++ player-select-backend/src/auth/auth.controller.ts
@@ -1,15 +1,18 @@
 import {Body, Controller, Post} from '@nestjs/common';
 import {PrismaService} from "../prisma/prisma.service";
 import {AuthLogInDto, AuthSigUpDto} from "./AuthDTO";
+import {AuthService} from "./auth.service";
 
 @Controller('auth')
 export class AuthController {
 
-    constructor(private prisma: PrismaService){}
+    constructor(
+        private authService: AuthService
+    ){}
 
     @Post('signUp')
     signUp(@Body() dto:AuthSigUpDto){
-
+        return this.authService.signUp(dto);
     }
 
     @Post('logIn')
--- player-select-backend/src/auth/auth.service.ts
+++ player-select-backend/src/auth/auth.service.ts
@@ -86,6 +86,9 @@ export class AuthService {
     }
 
 
+
+
+
     
 
 
