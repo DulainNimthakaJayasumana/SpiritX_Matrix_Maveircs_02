58f7ecddc750bfcc4f5e2365eea015339d55135b
Akash Welgama <139337308+Akashwelgama@users.noreply.github.com>
2025-03-09 12:13:31 +0530

    player-select-backend-version-04
    Auth functionality (Partial)

--- player-select-backend/src/auth/auth.controller.ts
+++ player-select-backend/src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
+import {PrismaService} from "../prisma/prisma.service";
+import {AuthLogInDto, AuthSigUpDto} from "./AuthDTO";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private prisma: PrismaService){}
+
+    @Post('signUp')
+    signUp(@Body() dto:AuthSigUpDto){
+
+    }
+
+    @Post('logIn')
+    logIn(@Body() dto: AuthLogInDto){
+
+    }
+
+
+}
--- player-select-backend/src/auth/auth.service.ts
+++ player-select-backend/src/auth/auth.service.ts
@@ -1,4 +1,95 @@
-import { Injectable } from '@nestjs/common';
+import {ForbiddenException, Injectable} from '@nestjs/common';
+import {PrismaService} from "../prisma/prisma.service";
+import {PrismaClientKnownRequestError} from "@prisma/client/runtime/client";
+import * as argon from 'argon2';
+import {AuthSigUpDto} from "./AuthDTO";
+import { JwtService } from '@nestjs/jwt';
+import {Role} from "@prisma/client";
+import {ConfigService} from "@nestjs/config";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private prisma: PrismaService,
+        private jwt: JwtService,
+        private config: ConfigService
+    ){}
+
+    async signUp(dto: AuthSigUpDto){
+
+        try {
+            const hash = await argon.hash(dto.password);
+
+            const user = await this.prisma.user.create({
+                data: {
+                    username: dto.username,
+                    password: hash,
+                    name: dto.name,
+                    role: dto.role,
+                },
+                select: {
+                    id: true,
+                    username: true,
+                    name: true,
+                    role: true,
+                }
+            });
+
+
+
+
+            return this.signToken(
+                user.id,
+                user.username,
+                user.name,
+                user.role
+            );
+
+        }catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken');
+                }
+            }
+            throw error;
+        }
+    }
+
+
+    async signToken(
+        sub: number,
+        username: string,
+        name: string,
+        role: Role,
+    ){
+
+        const payload = {
+            sub,
+            username,
+            role,
+        }
+
+        const secret = this.config.get('JWT_SECRET');
+
+        const token = await this.jwt.signAsync(
+            payload,
+            {
+                expiresIn: '15m',
+                secret,
+            });
+
+        return {
+          access_token:token,
+        };
+
+
+    }
+
+
+    
+
+
+
+
+
+}
